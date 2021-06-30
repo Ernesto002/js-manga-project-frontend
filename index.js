63658d955995fba7833ea782f67dcc5cdecb00c7
ernesto <ernesto002@icloud.com>
2021-06-30 16:36:38 -0700

    render collection to index/handle submit

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const base_url = "http://localhost:3000"
 const mangaService = new MangaService(base_url)
+const collectionService = new CollectionService(base_url)
 
 Manga.mangaForm.addEventListener("submit", handleMangaSubmit)
+Collection.collectionForm.addEventListener("submit", handleCollectionSubmit)
 
-mangaService.getMangas()
-Manga.renderMangaForm()
+collectionService.getCollections()
+Collection.renderCollectionForm()
+
+// mangaService.getMangas()
+// Manga.renderMangaForm()
 
 function handleMangaSubmit(){
     event.preventDefault()
     mangaService.createManga()
+    event.target.reset
+}
+
+function handleCollectionSubmit(){
+    event.preventDefault()
+    collectionService.createCollection()
     event.target.reset
 }
